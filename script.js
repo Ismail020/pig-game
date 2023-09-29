595ceb137fff4c0351c62a7e2dedad1d2c16280f
Ismail020 <isocetin1907@gmail.com>
2023-09-29 15:31:22 +0200

    Resetting the game

--- script.js
+++ script.js
@@ -13,15 +13,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlater, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlater = 0;
-let playing = true;
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlater = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlater}`).textContent = 0;
@@ -65,7 +78,7 @@ btnHold.addEventListener('click', function () {
     scores[activePlater];
 
   // 2. Check if player's score is >= 100
-  if (scores[activePlater] >= 20) {
+  if (scores[activePlater] >= 100) {
     // Finish the game
     playing = false;
     diceEl.classList.add('hidden');
@@ -80,3 +93,6 @@ btnHold.addEventListener('click', function () {
     switchPlayer();
   }
 });
+
+// Resetting the game
+btnNew.addEventListener('click', init);
